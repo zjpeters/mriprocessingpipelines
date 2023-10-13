81dce3ee6d454f1134d1c746fdd8d0d7c878402c
Zeru Peterson <zeru-peterson@uiowa.edu>
2023-10-13 13:59:02 -0500

    updated dicomDownload.sh to be a function so users don't have to save username and password information

--- mouse/generic/dicomDownload.sh
+++ mouse/generic/dicomDownload.sh
@@ -1,30 +1,22 @@
-# for (( i=0; i<${#DATE_LS[@]}; i++ )); do
-#   echo "DATE: " ${DATE_LS[${i}]}
-#   URL="https://rpacs.iibi.uiowa.edu/xnat/data/projects/${XNAT_PROJECT}/experiments?format=csv"
-#   curl -X GET -u ${UP} ${URL} -s --show-error \
-#     | awk -F "\"*,\"*" '{ print $2"\t"$7 }' \
-#     | grep "${DATE_LS[${i}]}" \
-#     > ${DIR_SAVE}"/"${XNAT_PROJECT}_${DATE_LS[${i}]}".pids"
-
-#   FNAME=${DIR_SAVE}"/"${XNAT_PROJECT}_${DATE_LS[${i}]}".pids"
-
-#   if [[ -s ${FNAME} ]]; then
-#     while read PID DT; do
-#       URL="https://rpacs.iibi.uiowa.edu/xnat/data/experiments/${PID}/scans/ALL/files?format=zip"
-#       curl -X GET -u $UP $URL --fail --silent --show-error \
-#       > ${DIR_SAVE}"/pi-"${PI}"_project-"${PROJECT}"_"${PID}"_"${DT//[-:. ]}".zip"
-#     done < ${DIR_SAVE}"/"${XNAT_PROJECT}_${DATE_LS[${i}]}".pids"
-#     rm ${DIR_SAVE}"/"${XNAT_PROJECT}_${DATE_LS[${i}]}".pids"
-#   else
-#     rm ${DIR_SAVE}"/"${XNAT_PROJECT}_${DATE_LS[${i}]}".pids"
-#   fi
-# done
-
+#!/bin/bash
+if [ $# != 2 ] ; then
+  echo "Usage: `basename $0` {XNAT_PROJECT} {sourcedata}"
+  echo "Function will request your username and password in order"
+  echo "to access XNAT repository"
+  exit 0;
+fi
 # the following downloads the information about the data contained in the project. we'll use this to help populate participants.tsv
-XNAT_PROJECT=TNJ_MOUSE
-UP= #"username:pw"
+# XNAT_PROJECT=TNJ_MOUSE
+XNAT_PROJECT=$1
+sourcedata=$2
+echo -n "Please enter XNAT username: "
+read username
+echo -n "Password: "
+read -s password
+
+UP="${username}:${password}"
 experimentUrl="https://rpacs.iibi.uiowa.edu/xnat/data/projects/${XNAT_PROJECT}/experiments?format=csv"
-curl -X GET -u ${UP} ${experimentUrl} --show-error > /media/zjpeters/Samsung_T5/functionalMouseTraining/sourcedata/${XNAT_PROJECT}.csv
+curl -X GET -u ${UP} ${experimentUrl} --show-error > $sourcedata/${XNAT_PROJECT}.csv
 
 
 # as is, this will download every image in the list, but changing the csv referenced in the line:
@@ -35,10 +27,10 @@ while IFS="," read accessOrder id project date xsiType label insertDate URI; do
     if [ "$label" == "3G_237-20220517" ]; then
     # if [ ! -f /media/zjpeters/Samsung_T5/functionalMouseTraining/sourcedata/${id}.zip ]; then
         imageDLUrl="https://rpacs.iibi.uiowa.edu/xnat"$URI"/scans/ALL/resources/DICOM/files?format=zip"
-        curl -X GET -u ${UP} ${imageDLUrl} --show-error >  /media/zjpeters/Samsung_T5/functionalMouseTraining/sourcedata/${id}.zip
+        curl -X GET -u ${UP} ${imageDLUrl} --show-error >  $sourcedata/${id}.zip
     else
         echo "File has already been downloaded for ${id}"
         continue
     fi
     # echo $imageDLUrl
-done < /home/zjpeters/rdss_tnj/twiceExceptional/sourcedata/${XNAT_PROJECT}.csv
+done < $sourcedata/${XNAT_PROJECT}.csv
