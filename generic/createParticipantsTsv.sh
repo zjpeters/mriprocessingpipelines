bdbab624d1e64ced0ac0b34e20b37a435c85dc41
zjpeters <zeru-peterson@uiowa.edu>
2024-03-27 12:54:14 -0500

    updated createParticipantsTsv.sh to fix string reading

--- generic/createParticipantsTsv.sh
+++ generic/createParticipantsTsv.sh
@@ -13,12 +13,12 @@ else
     touch "${rawdata}"/participants.tsv
     echo -e "participant_id" > "${rawdata}"/participants.tsv
     for subject in "${rawdata}"/sub-*; do
-        subID="$(basename $subject)"
+        subID="$(basename "${subject}")"
         echo -e "$subID" >> "${rawdata}"/participants.tsv
         touch "${subject}"/sessions.tsv
         echo -e "session_id" > "${subject}"/sessions.tsv
         for session in ${subject}/ses-*; do
-            sesID="$(basename $session)"
+            sesID="$(basename "${session}")"
             echo -e "$sesID" >> "${subject}"/sessions.tsv
         done
     done
--- human/anat/process7TAnatUpdateBiasFieldCorr.sh
+++ human/anat/process7TAnatUpdateBiasFieldCorr.sh
@@ -28,10 +28,14 @@ if [ ! -d $outputFolder ]; then
   3dresample -orient rpi -overwrite -prefix ${outputNameBase}_reorient_RPI.nii.gz -input $inputScan
   # not including mask in denoising since it hasn't been generated yet
   echo "Denoising $filename"
-  DenoiseImage -d 3 -n Rician -s 1 -p 1 -r 2 -v 1 -i ${outputNameBase}_reorient_RPI.nii.gz -o [${outputNameBase}_reorient_RPI_denoise.nii.gz,${outputNameBase}_reorient_RPI_noise.nii.gz]
+  # general N4 command used
+  echo "Running bias field correction on $filename"
+  N4BiasFieldCorrection -d 3 -i ${outputNameBase}_reorient_RPI.nii.gz -x ${templateMask} -r 1 -s 4 -c [50x50x50x50,0.0] -b [50,3] -o [${outputNameBase}_reorient_RPI_bfcorr.nii.gz,${outputNameBase}_reorient_RPI_bf.nii.gz]
+
+  DenoiseImage -d 3 -n Rician -s 1 -p 1 -r 2 -v 1 -i ${outputNameBase}_reorient_RPI_bfcorr.nii.gz -o [${outputNameBase}_reorient_RPI_bfcorr_denoise.nii.gz,${outputNameBase}_reorient_RPI_bfcorr_noise.nii.gz]
   # run antsRegistration on data
   echo "Registering $filename to $template"
-  antsRegistration --dimensionality 3 --output ${outputNameBase}_coreg --initial-moving-transform [${template},${outputNameBase}_reorient_RPI_denoise.nii.gz,1] \
+  antsRegistration --dimensionality 3 --output ${outputNameBase}_reorient_RPI_bfcorr_denoise_coreg --initial-moving-transform [${template},${outputNameBase}_reorient_RPI_bfcorr_denoise.nii.gz,1] \
     --transform Rigid[0.1] --metric Mattes[${template},${outputNameBase}_reorient_RPI_denoise.nii.gz,1,32,Regular,0.25] --convergence [2000x2000x2000x2000x2000,1e-6,10] --smoothing-sigmas 4x3x2x1x0vox --shrink-factors 8x8x4x2x1 \
     --transform Affine[0.1] --metric Mattes[${template},${outputNameBase}_reorient_RPI_denoise.nii.gz,1,32,Regular,0.25] --convergence [1000x1000x1000x1000x1000,1e-6,10] --smoothing-sigmas 4x3x2x1x0vox --shrink-factors 8x8x4x2x1 \
     --use-histogram-matching 1 --verbose 1 --random-seed 13983981 --winsorize-image-intensities [0.005,0.995] --write-composite-transform 1
@@ -39,13 +43,9 @@ if [ ! -d $outputFolder ]; then
   # add back in for nonlinear
   #--transform Syn[0.1,3,0] --metric CC[${template},${outputNameBase}_reorient_RPI_denoise.nii.gz,1,4,Regular,0.25] --convergence [1000x1000x1000x1000x1000,1e-6,10] --smoothing-sigmas 4x3x2x1x0vox --shrink-factors 8x8x4x2x1 \
 
-  antsApplyTransforms -d 3 -n BSpline[3] -i ${outputNameBase}_reorient_RPI_denoise.nii.gz -o ${outputNameBase}_reorient_RPI_denoise_coreg.nii.gz -t ${outputNameBase}_coregComposite.h5 -r $template
+  antsApplyTransforms -d 3 -n BSpline[3] -i ${outputNameBase}_reorient_RPI_bfcorr_denoise.nii.gz -o ${outputNameBase}_reorient_RPI_bfcorr_denoise_coreg.nii.gz -t ${outputNameBase}_reorient_RPI_bfcorr_denoise_coregComposite.h5 -r $template
 
-  # general N4 command used
-  echo "Running bias field correction on $filename"
-  N4BiasFieldCorrection -d 3 -i ${outputNameBase}_reorient_RPI_denoise_coreg.nii.gz -x ${templateMask} -r 1 -s 4 -c [50x50x50x50,0.0] -b [200,3] -t [0.15,0.01,200] -o [${outputNameBase}_reorient_RPI_denoise_coreg_bfcorr.nii.gz,${outputNameBase}_reorient_RPI_denoise_coreg_bf.nii.gz]
-
-elif [ -f ${outputNameBase}_reorient_RPI_denoise_coreg_bfcorr.nii.gz ]; then
+elif [ -f ${outputNameBase}_reorient_RPI_bfcorr_denoise.nii.gz ]; then
   echo "Preprocessing has already been run"
   exit 0
 fi
